e09f58fce560c038831b9cacc8f11c588bc7c97a
kaigidwani <kpg8244@rit.edu>
2025-01-26 13:42:22 -0500

    Added pe01. Added file organization with a folder for demos and a folder for PEs.

---
+++ PEs/gidwani-k-pe01/src/main.js
@@ -0,0 +1,34 @@
+"use strict";
+		
+// === Fields ===
+
+// The given arrays of technobabble
+const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+// Save the output paragraph
+let outputP = document.querySelector("#output");
+
+// Save the button
+let button = document.querySelector("#myButton");
+
+
+// Generate the technobabble for the first time when the page loads
+generateTechnobabble();
+
+
+// === Functions ===
+
+// Generates technobabble on the screen
+function generateTechnobabble()
+{
+    outputP.innerHTML = words1[Math.floor(Math.random() * words1.length)] + " "
+    + words2[Math.floor(Math.random() * words2.length)] + " "
+    + words3[Math.floor(Math.random() * words3.length)];
+}
+
+// When the button is clicked, generate technobabble
+button.addEventListener("click", generateTechnobabble);
