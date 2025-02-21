f126b53e6505a9187ab84c50c583db67802f20ce
Kai Gidwani (RIT Student) <kpg8244@rit.edu>
2025-02-21 09:51:53 -0500

    Made Friday demo exam study guide

---
+++ Demos/Week 6/friday-test.js
@@ -0,0 +1,21 @@
+const first_name = 'travis';
+const last_name = 'stodter';
+
+// Arrow functions, calls have to be made after it is declared
+
+// const make_full_name = (first, last) => {
+//     return `${first} ${last}, Maker of Names`;
+// }
+
+
+// This gets pulled to the top so it can be used at any time,
+// useful for utility functions at the end of files
+
+// function make_full_name(first, last) {
+//     return `${first} ${last}, Maker of Names`;
+// }
+
+export {
+    first_name,
+    make_full_name
+}
